6a54f33bd9f845ae210c6f84037be643c08d6b8d
Rustem <magistik.mag@yandex.ru>
2020-10-23 22:44:50 +0300

    correcting script

--- Project 1/script.js
+++ Project 1/script.js
@@ -1,19 +1,21 @@
-"use strict";
-
-let money = +prompt( "Ваш бюджет на месяц?" );
-let time = prompt( "Введите дату в формате YYYY-MM-DD" );
-
+let money = prompt("Ваш бюджет на месяц?", ''),
+	time = prompt('Введите дату в формате YYYY-MM-DD', '');
 
 let appData = {
-    money: money,
-    timeData: time,
-    expenses: {
-
-    },
-    optionalExpenses:{
-
-    },
-    income:[],
-    savings: false
+	budget: money,
+	expenses: {},
+	optionalExpenses: {},
+	income: [],
+	timeData: time,
+	savings: false
 };
-alert(appData.money / 30);
+
+let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	a2 = prompt("Во сколько обойдется?", ''),
+	a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	a4 = prompt("Во сколько обойдется?", '');
+
+appData.expenses.a1 = a2;
+appData.expenses.a3 = a4;
+
+alert(appData.budget / 30);
